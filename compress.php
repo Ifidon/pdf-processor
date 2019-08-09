c4c43479d6b391a250e8b1f5ca4ca79a10de598b
IFIDON OMOKHOMION <ifidon.omokhomion@internal.firs.gov.ng>
2019-08-09 12:20:49 +0100

    update1

--- compress.php
+++ compress.php
@@ -4,7 +4,7 @@ require_once  './ilovepdf-php/init.php';
 $n = 0;
 
 $usrip = $_SERVER["REMOTE_ADDR"];
-$pth = "./pdfuploads/".str_replace(":", "-", $usrip)."/";
+$pth = "c:/sites/pdfuploads/".str_replace(":", "-", $usrip)."/";
 
 is_dir($pth)?  : mkdir($pth, 0777, true);
 
@@ -48,7 +48,7 @@ foreach($files as $file) {
   $temp_file = $file['tmp_name'];
   try {
     move_uploaded_file($temp_file, $dest_file);
-    print_r("File uploaded successfully");
+    // print_r("File uploaded successfully");
   }
   catch(Execption $e) {
     echo($e->getMessage());
@@ -56,19 +56,31 @@ foreach($files as $file) {
 }
 
 $dload_pth = "./processed_files/".str_replace(":", "-", $usrip)."/compressed/";
+$file_n = "";
 if (!file_exists($dload_pth)) mkdir($dload_pth, 0777, true);
-$ilovepdf=new Ilovepdf('project_public_ee0f6b6af466b23bb3bade9c886411ab_jrhdt0cdbed1734a9e1501d1b6a0d10c5cc8b','secret_key_059af9b213174da160fbf7e8290ec261_9IRV72abd3ed98adc2a72a673b7ee0f5060ac', false);
-// $ilovepdf->verifySsl(false);
+$ilovepdf=new Ilovepdf('project_public_ee0f6b6af466b23bb3bade9c886411ab_jrhdt0cdbed1734a9e1501d1b6a0d10c5cc8b','secret_key_059af9b213174da160fbf7e8290ec261_9IRV72abd3ed98adc2a72a673b7ee0f5060ac');
+$ilovepdf->verifySsl(false);
 $myTask = $ilovepdf->newTask('compress');
+if(sizeof($save_path_array) > 1) {
+  $file_n = "merged_zip_".date("d-m-y h m sa").".zip";
+  $myTask->setPackagedFilename($file_n);
+}
+else if ($num) {
+  $file_n = $num.$filename;
+}
+else {
+  $file_n = $filename;
+}
 foreach($save_path_array as $file_path) {
   $file1=$myTask->addFile($file_path);
 }
 $myTask->execute();
-$myTask->download($dload_pth);
+$x = $myTask->download($dload_pth);
 
 
-$file_pth = $dload_pth.$num.$filename;
-$bname = basename($file_pth);
+$file_pth = $dload_pth.$file_n;
+$file_nm = basename($file_pth);
 
-echo($file_pth)
+// echo("<a href='$dload_pth' target='_blank'>Browse Folder</a>");
+echo("<a href='$file_pth' download>".$file_nm."</a>");
 ?>
